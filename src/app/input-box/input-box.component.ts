a85cab7e249d302bfae2e2b940bce026c0ed86ca
shwetabute <shwetabute@users.noreply.github.com>
2020-03-12 18:11:32 -0700

    project completed

--- src/app/input-box/input-box.component.ts
+++ src/app/input-box/input-box.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './input-box.component.html',
   styleUrls: ['./input-box.component.css']
 })
+
+
+
 export class InputBoxComponent{
   items = [];
   item = {
@@ -14,13 +17,30 @@ export class InputBoxComponent{
 
   constructor() { }
 
-  // function to add item in the items array 
+  // function to add or edit item in the items array 
   onClick(){
     if(this.item.id == 0){
-      (this.item.name.length>2) ? this.items.push({id: (new Date()).getTime(),name: this.capitalizeFirstLetter(this.item.name)}): "";
-      this.item.name = "";
+      (this.item.name.length>2) ? this.items.push({id: (new Date()).getTime(), name: this.capitalizeFirstLetter(this.item.name), strike: false}): "";
     }
+    else{
+      for(var i = 0;i < this.items.length; i++){
+        if(this.item.id == this.items[i].id){
+            this.items[i].name = this.capitalizeFirstLetter(this.item.name);
+            break;
+        }
+      } 
+    }
+    this.item.name = "";
+    this.item.id = 0;
   }
+
+
+  // function to edit item from the list
+  onEdit(e_item){
+      this.item.name = e_item.name;
+      this.item.id = e_item.id;
+  }
+
 
   // function to delete item from the list
   onDelete(d_item){
@@ -32,9 +52,24 @@ export class InputBoxComponent{
       }
   }
 
+
   // function to capitalize the first letter of the string
   capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  // function to mark an item as completed
+  onStrike(s_item) {
+      for(var i = 0;i < this.items.length; i++){
+          if(s_item.id == this.items[i].id){
+            if(this.items[i].strike){
+            this.items[i].strike = false;
+            }
+            else{
+              this.items[i].strike = true;
+            }
+          break;
+          }           
+      }
+  }
 }
